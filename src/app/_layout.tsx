02753f7b97d6071aca48df48ee4d802a74bb624d
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2024-10-19 20:27:33 -0700

    Creating root layout

---
+++ src/app/_layout.tsx
@@ -0,0 +1,27 @@
+import { Stack } from 'expo-router';
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(shop)"
+        options={{ headerShown: true, title: 'Shop' }}
+      />
+      <Stack.Screen
+        name="categories"
+        options={{ headerShown: true, title: 'Categories' }}
+      />
+      <Stack.Screen
+        name="product"
+        options={{ headerShown: true, title: 'Product' }}
+      />
+      {/* this will be a modal */}
+      <Stack.Screen
+        name="cart"
+        options={{ presentation: 'modal', title: 'Shopping Cart' }}
+      />
+      {/* will set a name later */}
+      <Stack.Screen name="auth" options={{ headerShown: true }} />
+    </Stack>
+  );
+}
