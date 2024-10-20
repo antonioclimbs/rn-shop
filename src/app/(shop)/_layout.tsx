587566c5043306b312c2813a31fb8663fe8f3bf8
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2024-10-19 20:41:03 -0700

    Updating (shop) layout and creating orders page and layout

--- src/app/(shop)/_layout.tsx
+++ src/app/(shop)/_layout.tsx
@@ -1,12 +1,16 @@
 import { Tabs } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const TabsLayout = () => {
   return (
-    <Tabs>
-      {/* the name refers to file/page in the folder */}
-      <Tabs.Screen name="index" options={{}} />
-      <Tabs.Screen name="orders" options={{}} />
-    </Tabs>
+    // SafeAreaView is a component that renders its children in a safe area that is guaranteed not to be obscured by the device's status bar, navigation bar, notch, or home indicator.
+    <SafeAreaView>
+      <Tabs>
+        {/* the name refers to file/page in the folder */}
+        <Tabs.Screen name="index" options={{ headerShown: false }} />
+        <Tabs.Screen name="orders" options={{}} />
+      </Tabs>
+    </SafeAreaView>
   );
 };
 
