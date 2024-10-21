6838b592d46e5f6530c80a64e0502451007880ed
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2024-10-21 12:23:57 -0700

    Adding a flat list and styles to shop home

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,13 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { PRODUCTS } from '../../../assets/products';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <View></View>}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={<Text>Products</Text>}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: 'space-between',
+  },
+});
