38c046a5223a7dbf9f55e0937956654a1b4dfb76
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2024-10-21 16:48:49 -0700

    Created product list item - takes in product as a prop. Updated shop index to use ProductListItem

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,12 +1,13 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { PRODUCTS } from '../../../assets/products';
+import { ProductListItem } from '../../components/product-list-item';
 
 const Home = () => {
   return (
     <View>
       <FlatList
         data={PRODUCTS}
-        renderItem={({ item }) => <View></View>}
+        renderItem={({ item }) => <ProductListItem product={item} />}
         keyExtractor={(item) => item.id.toString()}
         numColumns={2}
         ListHeaderComponent={<Text>Products</Text>}
