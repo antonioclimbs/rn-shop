c88cc3527cb5add139ce7a76fea0d734c197644a
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2024-10-21 17:20:11 -0700

    Created basic +not-found (404) page. Starting [slug] page

--- src/app/categories/[slug].tsx
+++ src/app/categories/[slug].tsx
@@ -1,6 +1,17 @@
+import { Redirect, useLocalSearchParams } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
+import { CATEGORIES } from '../../../assets/categories';
+import { PRODUCTS } from '../../../assets/products';
 
 const Category = () => {
+  const { slug } = useLocalSearchParams<{ slug: string }>();
+
+  const category = CATEGORIES.find((category) => category.slug === slug);
+
+  if (!category) return <Redirect href="/404" />;
+
+  const products = PRODUCTS.filter((product) => product.category.slug === slug);
+
   return (
     <View>
       <Text>Category</Text>
