3ae192021265f13dfd49b85789fadecd67dc52fe
Antonio Ayala <101016958+antonioclimbs@users.noreply.github.com>
2024-10-28 14:00:29 -0700

    auth page

--- src/app/auth.tsx
+++ src/app/auth.tsx
@@ -1,6 +1,21 @@
 import { Text, View } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import * as zod from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const authSchema = zod.object({
+  email: zod.string().email({ message: 'Invalid email address' }),
+  password: zod
+    .string()
+    .min(6, { message: 'Password must be at least 6 characters long' }),
+});
 
 export default function Auth() {
+  const {} = useForm({
+    resolver: zodResolver(authSchema),
+    defaultValues: { email: '', password: '' },
+  });
+
   return (
     <View>
       <Text>Auth</Text>
